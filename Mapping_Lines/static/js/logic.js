11e9c51923d8e9219ffb3e600fef33988bae1138
Donald Graham <donaldg1994@yahoo.com>
2021-07-24 19:49:54 -0400

    Mapping Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -5,7 +5,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and the second is longitude (-94.5).
 // We set the zoom level of "4" on a scale 0–18.
 
@@ -24,6 +24,33 @@ This method is useful when we need to add multiple tile layers, or a background 
 */
 // -------------------------------------------
 
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [34.0522, -118.2437],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
+let line2 = [
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
+
+L.polyline(line2, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: '5,5',
+  lineCap: "round"
+}).addTo(map);
+
 // Get data from cities.js
 let cityData = cities;
 
@@ -34,7 +61,7 @@ cityData.forEach(function(city) {
   L.circleMarker(city.location, {
     radius: city.population/200000,
     lineWeight: 4,
-    color: 'orange'
+    color: 'red'
   })
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 .addTo(map);
@@ -42,7 +69,7 @@ cityData.forEach(function(city) {
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
