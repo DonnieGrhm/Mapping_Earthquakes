b0b4eaa80d2bb67ac6ee60bba3643ecd04ab9e75
Donald Graham <donaldg1994@yahoo.com>
2021-07-24 16:18:24 -0400

    multiple points

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,41 @@
+//The console.log() function with the phrase "working" inside the parentheses will help us confirm that
+// our logic.js file is being accessed in the console on Chrome.
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([26, -80], 7);
+// The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and the second is longitude (-94.5).
+// We set the zoom level of "4" on a scale 0–18.
+
+//-----------------
+/*An alternative to using the setView() method is to modify each attribute in the map object using the curly braces notation as follows:
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 4
+});
+
+This method is useful when we need to add multiple tile layers, or a background image of our maps, which we will do later in this module.
+*/
+// -------------------------------------------
+
+//  Add a marker to the map for your hometown, Opa-Locka, Florida.
+L.circleMarker([25.9206, -80.2370], {
+  color: 'black',
+  fillColor: '#ffffa1',
+  radius:300
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
